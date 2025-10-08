0db1fb899d7d0cc7ac17230ebff4f73efba36dc8
David Kagan <david.kagan07@gmail.com>
2025-10-07 21:43:08 -0400

    wip: add to vault functionality

--- cmd/test_tview_root.go
+++ cmd/test_tview_root.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
@@ -11,7 +13,7 @@ import (
 	"go-pass/utils"
 )
 
-var helpText = "Commands"
+var helpText = "a: Add | Commands"
 
 // inputPassword string
 // passwordInput *tview.InputField
@@ -21,9 +23,10 @@ var helpText = "Commands"
 // CONFIG_FILE = path.Join(CONFIG_PATH, "gopass-cfg.json")
 
 type App struct {
-	App   *tview.Application
-	Vault []model.VaultEntry
-	Cfg   model.Config
+	App       *tview.Application
+	VaultFile *os.File
+	Vault     []model.VaultEntry
+	Cfg       model.Config
 
 	VaultList *tview.List
 	Root      *tview.Flex
@@ -41,8 +44,8 @@ func (a *App) PopulateVaultList() {
 	a.VaultList.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		// TODO: Add keys to proceed with vault actions
 		switch event.Rune() {
-		// case 'a':
-		// 	AddToVault()
+		case 'a':
+			a.ModalAddVault()
 		}
 		return event
 	})
@@ -82,6 +85,65 @@ func (a *App) ModalVaultInfo(idx int) *tview.Modal {
 	return modal
 }
 
+// TODO: Figure out which is better, returning a tview.Primitive, or nothing and
+// handling setting the root in the caller.
+// TODO: Figure out how to do this in a modal? Or maybe flex is just better.
+// And get this fucntionality working
+
+func (a *App) ModalAddVault() {
+	inputForm := tview.NewForm().
+		AddInputField("Name", "", 0, nil, nil).
+		AddInputField("Username", "", 0, nil, nil).
+		AddPasswordField("Password", "", 0, '*', nil).
+		AddInputField("Notes", "", 0, nil, nil)
+
+	inputForm.AddButton("Save", func() {
+		formName := inputForm.GetFormItemByLabel("Name").GetLabel()
+		formUsername := inputForm.GetFormItemByLabel("Username").GetLabel()
+		formPassword := inputForm.GetFormItemByLabel("Password").GetLabel()
+		formNotes := inputForm.GetFormItemByLabel("Notes").GetLabel()
+
+		fmt.Println("formName: ", formName)
+		fmt.Println("formUsername: ", formUsername)
+		fmt.Println("formPassword: ", formPassword)
+		fmt.Println("formNotes: ", formNotes)
+
+		a.App.SetRoot(a.Root, true)
+	})
+	inputForm.SetTitle("Add Vault")
+	inputForm.SetBorder(true)
+	inputForm.SetBackgroundColor(tcell.ColorBlack)
+	inputForm.SetFieldBackgroundColor(tcell.ColorBlack)
+	inputForm.SetButtonBackgroundColor(tcell.Color103)
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(nil, 0, 1, false).
+		AddItem(inputForm, 0, 1, true).
+		AddItem(nil, 0, 1, false)
+
+	a.App.SetRoot(flex, true)
+}
+
+func (a *App) AddToVault(name, notes, username string, password []byte) {
+	now := time.Now().UnixMilli()
+	vault := model.VaultEntry{
+		Name:      name,
+		Username:  username,
+		Notes:     notes,
+		Password:  password,
+		UpdatedAt: now,
+	}
+
+	a.Vault = append(a.Vault, vault)
+
+	encryptedCipherText, err := crypt.EncryptVault(a.Vault)
+	if err != nil {
+		panic(err)
+	}
+	utils.WriteToFile(a.VaultFile, encryptedCipherText)
+}
+
 func NewApp() *App {
 	return &App{}
 }
@@ -96,6 +158,7 @@ func TviewRun() {
 	app.Cfg = cfg
 
 	vaultF, _ := utils.OpenVault(cfg.VaultName)
+	app.VaultFile = vaultF
 	vault := crypt.DecryptVault(vaultF)
 	app.Vault = vault
 
