5b85e7384cb463c1674631e770a26eb5c66f9dd2
David Kagan <david.kagan07@gmail.com>
2025-12-31 12:40:57 -0500

    feat: add copy flag to CLI get command
    This adds the `--copy` flag to the `gopass vault get` command. This does
    not print the information to the terminal and copies the password to the
    clipboard

--- cmd/vault/get_test.go
+++ cmd/vault/get_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/atotto/clipboard"
 	"github.com/stretchr/testify/assert"
 
 	"go-pass/crypt"
@@ -62,8 +63,51 @@ func TestGetItemFromVault_HappyPath(t *testing.T) {
 	}, cfg, now, key)
 	assert.NoError(t, err)
 
-	err = GetItemFromVault(cfg, vaultEntry2, key)
+	err = GetItemFromVault(cfg, vaultEntry2, false, key)
 	assert.NoError(t, err)
+}
+
+func TestGetItemFromVault_CopyFlag(t *testing.T) {
+	testutils.TestCleanup(string(testutils.TEST_MASTER_PASSWORD))
+	defer testutils.TestCleanup(string(testutils.TEST_MASTER_PASSWORD))
+
+	key, err := testutils.InitTestKeyring(string(testutils.TEST_MASTER_PASSWORD))
+	assert.NoError(t, err)
+
+	cfgFile, err := utils.CreateConfig(
+		testutils.TEST_VAULT_NAME,
+		testutils.TEST_MASTER_PASSWORD,
+		testutils.TEST_CONFIG_NAME,
+		key,
+	)
+	assert.NoError(t, err)
+	defer cfgFile.Close()
+	vaultFile, err := utils.CreateVault(testutils.TEST_VAULT_NAME, key)
+	assert.NoError(t, err)
+	defer vaultFile.Close()
+
+	now := time.Now().UnixMilli()
+	cfg := &model.Config{
+		VaultName:      testutils.TEST_VAULT_NAME,
+		MasterPassword: testutils.TEST_MASTER_PASSWORD,
+		LastVisited:    now,
+	}
+
+	pass1, err := crypt.EncryptPassword([]byte(vaultEntry1), key)
+	assert.NoError(t, err)
+
+	err = AddToVault(vaultEntry1, model.UserInput{
+		Username: vaultEntry1,
+		Password: []byte(pass1),
+	}, cfg, now, key)
+	assert.NoError(t, err)
+
+	err = GetItemFromVault(cfg, vaultEntry1, true, key)
+	assert.NoError(t, err)
+
+	password, err := clipboard.ReadAll()
+	assert.NoError(t, err)
+	assert.Equal(t, vaultEntry1, password)
 }
 
 func TestGetItemFromVault_NotExist(t *testing.T) {
@@ -115,6 +159,6 @@ func TestGetItemFromVault_NotExist(t *testing.T) {
 	}, cfg, now, key)
 	assert.NoError(t, err)
 
-	err = GetItemFromVault(cfg, "notExist", key)
+	err = GetItemFromVault(cfg, "notExist", false, key)
 	assert.Error(t, err)
 }
