a508575bb2ee56ab523a1cfe663acb479d64d35c
David Kagan <102766847+DKagan07@users.noreply.github.com>
2026-01-04 20:57:18 -0500

    chore: cleanup the `TivewRun` function (#15)
    Making that func cleaner, moved the creation of the login page to a
    separate function and file

--- cmd/tui/run.go
+++ cmd/tui/run.go
@@ -1,17 +1,12 @@
 package tui
 
 import (
-	"fmt"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 
-	"go-pass/crypt"
 	"go-pass/model"
-	"go-pass/utils"
 )
 
 var HelpText = "a: Add | d: Delete | u: Update | g: Generate Password | b: Create backup | l: Toggle Backup Display | q: Quit | tab: Switch Search and Vault"
@@ -70,86 +65,7 @@ func (a *App) CreateRoot() *tview.Flex {
 func TviewRun() {
 	app := NewApp()
 	app.App = tview.NewApplication()
-	var loginPage *tview.Flex
-
-	// if !app.IsLoggedIn {
-	loginForm := tview.NewForm().
-		AddPasswordField("Master Password", "", 0, '*', nil)
-
-	loginForm.SetTitle(" Login ")
-	loginForm.SetBorder(true)
-	loginForm.SetBackgroundColor(tcell.ColorBlack)
-	loginForm.SetFieldBackgroundColor(tcell.ColorBlack)
-	loginForm.SetButtonBackgroundColor(tcell.Color103)
-	loginForm.AddButton("Login", func() {
-		masterPassword := loginForm.GetFormItem(0).(*tview.InputField).GetText()
-
-		keyring := model.NewMasterAESKeyManager(masterPassword)
-		app.Keyring = keyring
-
-		cfgFile, ok, err := utils.OpenConfig("")
-		if ok && err == nil {
-			modal := app.ExitErrorModal("a file is not found. need to run 'gopass init'")
-			app.App.SetRoot(modal, true)
-		}
-
-		cfg, err := crypt.DecryptConfig(cfgFile, app.Keyring, false)
-		if err != nil {
-			errMsg := err.Error()
-			if strings.Contains(errMsg, "decrypting contents") {
-				app.NumRetries++
-				errMsg = fmt.Sprintf("Authentication Failed, retries: %d", app.NumRetries)
-			}
-
-			if app.NumRetries >= 3 {
-				finalModal := app.ExitErrorModal("All attempts made")
-				app.App.SetRoot(finalModal, true)
-				return
-			} else {
-				modal := app.ErrorModal(errMsg, loginPage)
-				app.App.SetRoot(modal, true)
-				return
-			}
-		}
-
-		app.Cfg = cfg
-
-		vaultF, err := utils.OpenVault(cfg.VaultName)
-		if err != nil {
-			modal := app.ErrorModal(err.Error(), loginPage)
-			app.App.SetRoot(modal, true)
-			return
-		}
-
-		app.VaultFile = vaultF
-		vault, err := crypt.DecryptVault(vaultF, app.Keyring, false)
-		if err != nil {
-			modal := app.ExitErrorModal(err.Error())
-			app.App.SetRoot(modal, true)
-		}
-
-		app.Vault = vault
-		app.FilteredVault = vault
-
-		app.PopulateVaultList()
-
-		root := app.CreateRoot()
-		app.Root = root
-
-		cfg.LastVisited = time.Now().UnixMilli()
-		encryptedCfg, err := crypt.EncryptConfig(cfg, app.Keyring)
-		if err != nil {
-			modal := app.ExitErrorModal(err.Error())
-			app.App.SetRoot(modal, true)
-		}
-		utils.WriteToFile(cfgFile, encryptedCfg)
-
-		app.App.SetRoot(app.Root, true)
-	})
-
-	loginPage = tview.NewFlex().
-		SetDirection(tview.FlexRow).
-		AddItem(loginForm, 0, 1, true)
+	loginPage := app.LoginPage()
 
 	if err := app.App.SetRoot(loginPage, true).Run(); err != nil {
 		modal := app.ExitErrorModal(err.Error())
